8ac600a8d968b215bf693029aaf830b3d0c70e1f
Allen Cao <allen.m.cao@gmail.com>
2015-08-15 23:02:26 -0700

    redo is same as undo, disabled if unable to redo ALSO tab->spaces

--- src/notepadextended/SynWrite.java
+++ src/notepadextended/SynWrite.java
@@ -1,7 +1,7 @@
 package notepadextended;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+// import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,236 +29,239 @@ import javax.swing.undo.UndoManager;
 //base operations inspired fromhttp://codereview.stackexchange.com/questions/51175/simple-text-editor-classand
 //http://forum.codecall.net/topic/49721-simple-text-editor/
 
-public class SynWrite implements ActionListener{
+public class SynWrite implements ActionListener {
 
-	public static void main (String[] args) {
-		new SynWrite();
-	}
-	
-	private JMenuBar menuBar;
-	private JMenu file, edit;
-	private JMenuItem fileNew, fileOpen, fileSave, fileSaveAs, fileExit;
-	private JMenuItem editCopy, editPaste, editCut, editSelectAll, editUndo,
-	editRedo;
-	private JFrame window;
-	private JTextArea textArea;
-	private JScrollPane scrollPane;
-	private Border textBorder; // none so far
-	private Font textFont;
-	private JToolBar toolBar; // needs to be implemented
-	private boolean opened;
-	private boolean saved;
-	private boolean changed;
-	private File openedFile;
-	private UndoManager uManager;
-	
-	public SynWrite() {
-		createTextArea();
-		createUndoManager();
-		createWindow();
-	}
-	
-	public JFrame createWindow() {
-		window = new JFrame("New File");
-		window.setVisible(true);
-		window.setJMenuBar(createMenuBar());
-		window.add(scrollPane, BorderLayout.CENTER);
-		window.pack();
-		window.setLocationRelativeTo(null);
-		return window;
-	}
-	
-	public JMenuBar createMenuBar() {
-		menuBar = new JMenuBar();
-		menuBar.add(createFile());
-		menuBar.add(createEdit());
+    public static void main(String[] args) {
+        new SynWrite();
+    }
 
-		return menuBar;
-	}
-	
-	public JTextArea createTextArea() {
-		textArea = new JTextArea(30, 50);
-		textArea.setEditable(true);
-		textArea.setBorder(BorderFactory.createCompoundBorder(textBorder,
-				BorderFactory.createEmptyBorder(2, 5, 0, 0)));
+    private JMenuBar menuBar;
+    private JMenu file, edit;
+    private JMenuItem fileNew, fileOpen, fileSave, fileSaveAs, fileExit;
+    private JMenuItem editCopy, editPaste, editCut, editSelectAll, editUndo,
+            editRedo;
+    private JFrame window;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
+    private Border textBorder; // none so far
+    private Font textFont;
+    private JToolBar toolBar; // needs to be implemented
+    private boolean opened;
+    private boolean saved;
+    private boolean changed;
+    private File openedFile;
+    private UndoManager uManager;
 
-		textFont = new Font("Lucida Console", 0, 12);
-		textArea.setFont(textFont);
+    public SynWrite() {
+        createTextArea();
+        createUndoManager();
+        createWindow();
+    }
 
-		scrollPane = new JScrollPane(textArea);
+    public JFrame createWindow() {
+        window = new JFrame("New File");
+        window.setVisible(true);
+        window.setJMenuBar(createMenuBar());
+        window.add(scrollPane, BorderLayout.CENTER);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        return window;
+    }
 
-		return textArea;
-	}
-	
-	private UndoManager createUndoManager() {
-		uManager = new UndoManager() {
-			@Override
-			public void undoableEditHappened(UndoableEditEvent e) {
-				super.undoableEditHappened(e);
-				editUndo.setEnabled(true);
-				if (!changed) {
-					window.setTitle("*" + window.getTitle());
-					changed = true;
-				}
-			}
-		};
-		textArea.getDocument().addUndoableEditListener(uManager);
-		return uManager;
-	}
-	
-	public JMenu createFile() {
-		file = new JMenu("File");
-		fileNew = new JMenuItem("New");
-		fileNew.addActionListener(this);
-		fileOpen = new JMenuItem("Open");
-		fileOpen.addActionListener(this);
-		fileSave = new JMenuItem("Save");
-		fileSave.addActionListener(this);
-		fileSaveAs = new JMenuItem("SaveAs");
-		fileSaveAs.addActionListener(this);
-		fileExit = new JMenuItem("Exit");
-		fileExit.addActionListener(this);
-		file.add(fileNew);
-		file.add(fileOpen);
-		file.add(fileSave);
-		file.add(fileSaveAs);
-		file.add(fileExit);
-		return file;
-	}
-	
-	public JMenu createEdit() {
-		edit = new JMenu("Edit");
-		editUndo = new JMenuItem("Undo");
-		editUndo.addActionListener(this);
-		editUndo.setEnabled(false);
-		editRedo = new JMenuItem("Redo");
-		editRedo.addActionListener(this);
-		editUndo.setEnabled(false);
-		editSelectAll = new JMenuItem("Select All");
-		editSelectAll.addActionListener(this);
-		editCopy = new JMenuItem("Copy");
-		editCopy.addActionListener(this);
-		editCut = new JMenuItem("Cut");
-		editCut.addActionListener(this);
-		editPaste = new JMenuItem("Paste");
-		editPaste.addActionListener(this);
-		edit.add(editUndo);
-		edit.add(editRedo);
-		edit.add(editSelectAll);
-		edit.add(editCopy);
-		edit.add(editCut);
-		edit.add(editPaste);
-		return edit;
-	}
-	
-	private void saveFile(File filename) {
-		try {
-			BufferedWriter writer = new BufferedWriter(new
-					FileWriter(filename));
-			writer.write(textArea.getText());
-			writer.close();
-			saved = true;
-			changed = false;
-			window.setTitle(filename.getName());
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
+    public JMenuBar createMenuBar() {
+        menuBar = new JMenuBar();
+        menuBar.add(createFile());
+        menuBar.add(createEdit());
 
-	private void quickSave(File filename) {
-		try {
-			BufferedWriter writer = new BufferedWriter(new
-					FileWriter(filename));
-			writer.write(textArea.getText());
-			writer.close();
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
+        return menuBar;
+    }
 
-	private void openingFiles(File filename) {
-		try {
-			openedFile = filename;
-			FileReader reader = new FileReader(filename);
-			textArea.read(reader, null);
-			opened = true;
-			window.setTitle(filename.getName());
+    public JTextArea createTextArea() {
+        textArea = new JTextArea(30, 50);
+        textArea.setEditable(true);
+        textArea.setBorder(BorderFactory.createCompoundBorder(textBorder,
+                BorderFactory.createEmptyBorder(2, 5, 0, 0)));
 
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
+        textFont = new Font("Lucida Console", 0, 12);
+        textArea.setFont(textFont);
 
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		if(event.getSource() == fileNew) {
-			new SynWrite();
-		} else if(event.getSource() == fileOpen) {
-			JFileChooser openChooser = new JFileChooser();
-			openChooser.showOpenDialog(null);
-			File file = openChooser.getSelectedFile();
-			openingFiles(file);
-		} else if(event.getSource() == fileSave) {
-			JFileChooser saveChooser = new JFileChooser();
-			File filename = saveChooser.getSelectedFile();
-			if(opened == false && saved == false) {
-				saveChooser.showSaveDialog(null);
-				int confirmationResult;
-				if(filename.exists()) {
-					confirmationResult =
-							JOptionPane.showConfirmDialog(fileSave, "Replace existing file?");
-					if(confirmationResult == JOptionPane.YES_OPTION) {
-						saveFile(filename);
-					}
-				} else {
-					saveFile(filename);
-				}
-			} else {
-				quickSave(openedFile);
-			}
-		} else if(event.getSource() == fileSaveAs) {
-			JFileChooser saveAs = new JFileChooser();
-			saveAs.showSaveDialog(null);
-			File filename = saveAs.getSelectedFile();
-			int confirmationResult;
-			if(filename.exists()) {
-				confirmationResult =
-						JOptionPane.showConfirmDialog(fileSaveAs, "Replace existing file?");
-				if(confirmationResult == JOptionPane.YES_OPTION) {
-					saveFile(filename);
-				}
-			} else {
-				saveFile(filename);
-			}
-		} else if(event.getSource() == fileExit) {
-			System.exit(0);
-		} else if(event.getSource() == editUndo) {
-			try {
-				uManager.undo();
-				if (!uManager.canUndo()) {
-					editUndo.setEnabled(false);
-				}
-			} catch(CannotUndoException cu) {
-				cu.printStackTrace();
-			}
-		} else if(event.getSource() == editRedo) {
-			try {
-				uManager.redo();
-			} catch(CannotUndoException cur) {
-				cur.printStackTrace();
-			}
-		} else if(event.getSource() == editSelectAll) {
-			textArea.selectAll();
-		}  else if(event.getSource() == editCopy) {
-			textArea.copy();
-		} else if(event.getSource() == editPaste) {
-			textArea.paste();
-		} else if(event.getSource() == editCut) {
-			textArea.cut();
-		}
-	}
+        scrollPane = new JScrollPane(textArea);
 
-	public void setTextArea(JTextArea text) {
-		textArea = text;
-	}
+        return textArea;
+    }
+
+    private UndoManager createUndoManager() {
+        uManager = new UndoManager() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                super.undoableEditHappened(e);
+                editUndo.setEnabled(true);
+                if (!changed) {
+                    window.setTitle("*" + window.getTitle());
+                    changed = true;
+                }
+            }
+        };
+        textArea.getDocument().addUndoableEditListener(uManager);
+        return uManager;
+    }
+
+    public JMenu createFile() {
+        file = new JMenu("File");
+        fileNew = new JMenuItem("New");
+        fileNew.addActionListener(this);
+        fileOpen = new JMenuItem("Open");
+        fileOpen.addActionListener(this);
+        fileSave = new JMenuItem("Save");
+        fileSave.addActionListener(this);
+        fileSaveAs = new JMenuItem("SaveAs");
+        fileSaveAs.addActionListener(this);
+        fileExit = new JMenuItem("Exit");
+        fileExit.addActionListener(this);
+        file.add(fileNew);
+        file.add(fileOpen);
+        file.add(fileSave);
+        file.add(fileSaveAs);
+        file.add(fileExit);
+        return file;
+    }
+
+    public JMenu createEdit() {
+        edit = new JMenu("Edit");
+        editUndo = new JMenuItem("Undo");
+        editUndo.addActionListener(this);
+        editUndo.setEnabled(false);
+        editRedo = new JMenuItem("Redo");
+        editRedo.addActionListener(this);
+        editRedo.setEnabled(false);
+        editSelectAll = new JMenuItem("Select All");
+        editSelectAll.addActionListener(this);
+        editCopy = new JMenuItem("Copy");
+        editCopy.addActionListener(this);
+        editCut = new JMenuItem("Cut");
+        editCut.addActionListener(this);
+        editPaste = new JMenuItem("Paste");
+        editPaste.addActionListener(this);
+        edit.add(editUndo);
+        edit.add(editRedo);
+        edit.add(editSelectAll);
+        edit.add(editCopy);
+        edit.add(editCut);
+        edit.add(editPaste);
+        return edit;
+    }
+
+    private void saveFile(File filename) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write(textArea.getText());
+            writer.close();
+            saved = true;
+            changed = false;
+            window.setTitle(filename.getName());
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    private void quickSave(File filename) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write(textArea.getText());
+            writer.close();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    private void openingFiles(File filename) {
+        try {
+            openedFile = filename;
+            FileReader reader = new FileReader(filename);
+            textArea.read(reader, null);
+            opened = true;
+            window.setTitle(filename.getName());
+
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == fileNew) {
+            new SynWrite();
+        } else if (event.getSource() == fileOpen) {
+            JFileChooser openChooser = new JFileChooser();
+            openChooser.showOpenDialog(null);
+            File file = openChooser.getSelectedFile();
+            openingFiles(file);
+        } else if (event.getSource() == fileSave) {
+            JFileChooser saveChooser = new JFileChooser();
+            File filename = saveChooser.getSelectedFile();
+            if (opened == false && saved == false) {
+                saveChooser.showSaveDialog(null);
+                int confirmationResult;
+                if (filename.exists()) {
+                    confirmationResult = JOptionPane.showConfirmDialog(
+                            fileSave, "Replace existing file?");
+                    if (confirmationResult == JOptionPane.YES_OPTION) {
+                        saveFile(filename);
+                    }
+                } else {
+                    saveFile(filename);
+                }
+            } else {
+                quickSave(openedFile);
+            }
+        } else if (event.getSource() == fileSaveAs) {
+            JFileChooser saveAs = new JFileChooser();
+            saveAs.showSaveDialog(null);
+            File filename = saveAs.getSelectedFile();
+            int confirmationResult;
+            if (filename.exists()) {
+                confirmationResult = JOptionPane.showConfirmDialog(fileSaveAs,
+                        "Replace existing file?");
+                if (confirmationResult == JOptionPane.YES_OPTION) {
+                    saveFile(filename);
+                }
+            } else {
+                saveFile(filename);
+            }
+        } else if (event.getSource() == fileExit) {
+            System.exit(0);
+        } else if (event.getSource() == editUndo) {
+            try {
+                uManager.undo();
+                if (!uManager.canUndo()) {
+                    editUndo.setEnabled(false);
+                }
+                editRedo.setEnabled(true);
+            } catch (CannotUndoException cu) {
+                cu.printStackTrace();
+            }
+        } else if (event.getSource() == editRedo) {
+            try {
+                uManager.redo();
+                if (!uManager.canRedo()) {
+                    editRedo.setEnabled(false);
+                }
+                editUndo.setEnabled(true);
+            } catch (CannotUndoException cur) {
+                cur.printStackTrace();
+            }
+        } else if (event.getSource() == editSelectAll) {
+            textArea.selectAll();
+        } else if (event.getSource() == editCopy) {
+            textArea.copy();
+        } else if (event.getSource() == editPaste) {
+            textArea.paste();
+        } else if (event.getSource() == editCut) {
+            textArea.cut();
+        }
+    }
+
+    public void setTextArea(JTextArea text) {
+        textArea = text;
+    }
 }
